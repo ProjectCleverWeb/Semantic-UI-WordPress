d1aadc8fb9d8880ab54407dee941f42161005d10
NULL <administrator@projectcleverweb.com>
2013-11-06 11:13:08 -0500

    Finished: menu->display()

--- lib/scripts/objects/semantic_ui-menu.class.php
+++ lib/scripts/objects/semantic_ui-menu.class.php
@@ -110,21 +110,42 @@ class menu {
 			}
 			
 			// Build item
-			if (empty($url) || isset($menu_item['children'])) {
-				$fmt = $conf['before_item'].'<div %1$sid="id-%2$s" %3$s%5$s>%4$s%7$s</div>'.$conf['after_item'];
+			$children = '';
+			if (empty($url)) {
+				$fmt = '<div %1$s%2$s>'.$conf['before_item'].'<span %3$s>%4$s</span>'.$conf['after_item'].'%5$s</div>';
+				if (isset($menu_item['children'])) {
+					$children = $this->_display_get_children($menu_item['children'],$conf);
+					$text = $text.'<i class="dropdown icon"></i>';
+				}
+				
+				$items .= sprintf($fmt,
+					$classes,
+					'id="'.$id.'"',
+					$title,
+					$conf['before_text'].$text.$conf['after_text'],
+					$children
+				).PHP_EOL;
 			} else {
-				$fmt = $conf['before_item'].'<a %1$sid="id-%2$s" %3$s%5$shref="%6$s">%4$s</a>'.$conf['after_item'];
+				$link_attr = sprintf('%1$s%2$s%3$s',
+					$target,
+					$title,
+					'href="'.$url.'"'
+				);
+				
+				$fmt = '<div %1$s%2$s>'.$conf['before_item'].'<a %3$s>%4$s</a>'.$conf['after_item'].'%5$s</div>';
+				if (isset($menu_item['children'])) {
+					$children = $this->_display_get_children($menu_item['children'],$conf);
+					$text = $text.'<i class="dropdown icon"></i>';
+				}
+				
+				$items .= sprintf($fmt,
+					$classes,
+					'id="'.$id.'"',
+					$link_attr,
+					$conf['before_text'].$text.$conf['after_text'],
+					$children
+				).PHP_EOL;
 			}
-			$items .= sprintf($fmt,
-				$classes,
-				$id,
-				$target,
-				$conf['before_text'].$text.$conf['after_text'],
-				$title,
-				$url,
-				$children
-			).PHP_EOL;
-			
 		}
 		
 		// now display
@@ -132,6 +153,118 @@ class menu {
 		
 	}
 	
+	public function _display_get_children($children,$conf){
+		static $depth;
+		if (!$depth) {
+			$depth = 0;
+		}
+		$depth++;
+		
+		$items   = PHP_EOL;
+		foreach ($children as $menu_item) {
+			// build attributes
+			$classes    = '';
+			$id         = $menu_item['ID'];
+			$item_id    = trim(sprintf($conf['item_id'], $id));
+			$target     = '';
+			$text       = '';
+			$title      = '';
+			$url        = $menu_item['url'];
+			
+			if (!empty($menu_item['classes']) && is_array($menu_item['classes'])) {
+				foreach ($menu_item['classes'] as $class) {
+					$class = trim($class);
+					if (!empty($class)) {
+						$classes .= $class.' ';
+					}
+				}
+				$classes = trim(sprintf($conf['item_class'], trim($classes)));
+				
+				if (isset($menu_item['children'])) {
+					$classes = trim('ui simple dropdown item '.$classes);
+				} else {
+					$classes = trim('item '.$classes);
+				}
+				
+				$classes = "class=\"$classes\" ";
+			}
+			
+			if (!$conf['no_target'] && !empty($menu_item['target'])) {
+				$target = 'target="'.$menu_item['target'].'" ';
+			}
+			
+			if (!empty($menu_item['attr_title'])) {
+				$text = $menu_item['attr_title'];
+			} elseif (!empty($menu_item['decription'])) {
+				$text = $menu_item['description'];
+			} elseif (!empty($menu_item['title'])) {
+				$text = $menu_item['title'];
+			} elseif (!empty($menu_item['post_title'])) {
+				$text = $menu_item['post_title'];
+			} elseif (!empty($menu_item['post_excerpt'])) {
+				$text = $menu_item['post_excerpt'];
+			}
+			
+			if (!empty($menu_item['attr_title'])) {
+				$title = 'title="'.esc_attr__($menu_item['attr_title']).'" ';
+			} elseif (!empty($menu_item['decription'])) {
+				$title = 'title="'.esc_attr__($menu_item['description']).'" ';
+			} elseif (!empty($menu_item['post_excerpt'])) {
+				$title = 'title="'.esc_attr__($menu_item['post_excerpt']).'" ';
+			} elseif (!empty($menu_item['title'])) {
+				$title = 'title="'.esc_attr__($menu_item['title']).'" ';
+			} elseif (!empty($menu_item['post_title'])) {
+				$title = 'title="'.esc_attr__($menu_item['post_title']).'" ';
+			}
+			
+			$children = '';
+			
+			if (isset($menu_item['children'])) {
+				// handle children
+			}
+			
+			// Build item
+			$children = '';
+			if (empty($url)) {
+				$fmt = '<div %1$s%2$s>'.$conf['before_item'].'<span %3$s>%4$s</span>'.$conf['after_item'].'%5$s</div>';
+				if (isset($menu_item['children'])) {
+					$children = $this->_display_get_children($menu_item['children'],$conf);
+				}
+				
+				$items .= sprintf($fmt,
+					$classes,
+					'id="'.$id.'"',
+					$title,
+					$conf['before_text'].$text.$conf['after_text'],
+					$children
+				).PHP_EOL;
+			} else {
+				$link_attr = sprintf('%1$s%2$s%3$s',
+					$target,
+					$title,
+					'href="'.$url.'"'
+				);
+				
+				$fmt = '<div %1$s%2$s>'.$conf['before_item'].'<a %3$s>%4$s</a>'.$conf['after_item'].'%5$s</div>';
+				if (isset($menu_item['children'])) {
+					$children = $this->_display_get_children($menu_item['children'],$conf);
+				}
+				
+				$items .= sprintf($fmt,
+					$classes,
+					'id="'.$id.'"',
+					$link_attr,
+					$conf['before_text'].$text.$conf['after_text'],
+					$children
+				).PHP_EOL;
+			}
+		}
+		
+		
+		
+		$depth--;
+		return "<div class=\"menu\" id=\"$the_id\">$items</div>".PHP_EOL;
+	}
 	
 	public function get($menu_id = FALSE) {
 		$ref = &$this->ref;
