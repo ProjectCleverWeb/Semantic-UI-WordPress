013968474e8c9ad651c3a5caa32c6eea2c3d453c
Nicholas Jordon <projectcleverweb@gmail.com>
2014-11-19 16:51:17 -0500

    fixed theme options page tabs

--- src/assets/scripts/theme-options.js
+++ src/assets/scripts/theme-options.js
@@ -1,35 +1,6 @@
 // By default, jQuery in the WordPress dashboard runs in "safe mode" which means
 // the '$' shortcut is not available. Since these scripts are only run in the
 // dashboard, any tracking should be disabled/removed.
-
-function theme_options_display(name, is_default) {
-	is_default = typeof is_default !== 'undefined' ? is_default : false;
-	if (is_default) {
-		sections = jQuery('.theme-options-section');
-		the_default = jQuery('#theme-options-' + name);
-		if (the_default.length) {
-			jQuery('#theme-options-menu').css('display', 'block');
-			jQuery('h3.section-header').css('display', 'none');
-			sections.css('display', 'none');
-			the_default.css('display', '');
-			jQuery('.theme-options-menu .' + name + '.item').addClass('active');
-		} else {
-			console.log('theme_options_display() says: ".theme-options-section.' + name + '" does not exist! did nothing...');
-		}
-		return;
-	} else {
-		sections = jQuery('.theme-options-section');
-	}
-	section = jQuery('#theme-options-' + name);
-	if (section.length && !jQuery('.theme-options-menu .' + name + '.item.active').length) {
-		sections.css('display', 'none');
-		section.css('display', '');
-		section.transition('fade in');
-		jQuery('.theme-options-menu .item').removeClass('active');
-		jQuery('.theme-options-menu .' + name + '.item').addClass('active');
-	};
-}
-
 jQuery(document).ready(function() {
 	// Semantic UI inits
 	jQuery('.ui.accordion').accordion();
@@ -48,9 +19,9 @@ jQuery(document).ready(function() {
 		jQuery(this).closest('.message').fadeOut();
 	});
 	
-	
-	theme_options_display('general', true);
-	
+	jQuery('#theme-options-tabs .menu .item').tab({
+		context: jQuery('#theme-options-tabs')
+	});
 	
 	// don't show invalid images
 	jQuery("img").error(function () { 
--- src/contents/theme-options.php
+++ src/contents/theme-options.php
@@ -1,41 +1,32 @@
-<div class="ui tabular top attached menu theme-options-menu" style="display:none" id="theme-options-menu">
-	<a class="general item" href="javascript: theme_options_display('general');">
-		<i class="wrench icon"></i>
-		General
-	</a>
-	<a class="meta-tags item" href="javascript: theme_options_display('meta-tags');">
-		<i class="setting icon"></i>
-		Meta Tags
-	</a>
-	<a class="about item" href="javascript: theme_options_display('about');">
-		<i class="book icon"></i>
-		About
-	</a>
-</div>
-
-<form class="ui bottom attached segment" method="POST" action="<?php echo theme::options_uri(); ?>">
-	<?php
-	// This makes sure that the changes will be accepted
-	theme::options_update_data();
-	?>
+<div id="theme-options-tabs">
 	
-	<section class="theme-options-section" id="theme-options-general">
-		<h3 class="ui inverted black block header section-header">General</h3>
-		<?php theme::part('content-general', 'content', 'theme-options-general'); ?>
-	</section>
-	<section class="theme-options-section" id="theme-options-meta-tags">
-		<h3 class="ui inverted black block header section-header">Meta Tags</h3>
-		<?php theme::part('content-meta-tags', 'content', 'theme-options-meta-tags'); ?>
-	</section>
-	<section class="theme-options-section" id="theme-options-about">
-		<h3 class="ui inverted black block header section-header">About</h3>
-		<?php theme::part('content-about', 'content', 'theme-options-about'); ?>
-	</section>
-	
-	<br>
-	
-	<div class="ui buttons">
-		<a class="ui button" href="<?php echo theme::options_uri(); ?>">Cancel</a>
-		<button type="submit" class="ui positive submit button">Save All</button>
+	<div class="ui top attached tabular menu">
+		<a class="item active" data-tab="first"><i class="wrench icon"></i> General</a>
+		<a class="item" data-tab="second"><i class="setting icon"></i> Meta Tags</a>
+		<a class="item" data-tab="third"><i class="book icon"></i> About</a>
 	</div>
-</form>
+	
+	<form class="ui bottom attached segment" method="POST" action="<?php echo theme::options_uri(); ?>">
+		<?php
+		// This makes sure that the changes will be accepted
+		theme::options_update_data();
+		?>
+		
+		<section class="ui tab active" data-tab="first">
+			<?php theme::part('content-general', 'content', 'theme-options-general'); ?>
+		</section>
+		<section class="ui tab" data-tab="second">
+			<?php theme::part('content-meta-tags', 'content', 'theme-options-meta-tags'); ?>
+		</section>
+		<section class="ui tab" data-tab="third">
+			<?php theme::part('content-about', 'content', 'theme-options-about'); ?>
+		</section>
+		
+		<br>
+		
+		<div class="ui buttons">
+			<a class="ui button" href="<?php echo theme::options_uri(); ?>">Cancel</a>
+			<button type="submit" class="ui positive submit button">Save All</button>
+		</div>
+	</form>
+</div>
