fadcb6a595994eb1af3b1e7bcb2670d8ab1f3d85
Nicholas Jordon <projectcleverweb@gmail.com>
2014-12-04 22:52:08 -0500

    moved autoloader from functions.php to includes/autoload.php

--- src/functions.php
+++ src/functions.php
@@ -3,29 +3,10 @@
  * Semanitic UI for WordPress functions file
  */
 
-/**
- * Simple PSR-4 autoloader for the \semantic namespace
- * 
- * Loads files from `includes/classes`
- * 
- * @codeCoverageIgnore
- */
-spl_autoload_register(function ($class) {
-	$prefix = 'semantic';
-	
-	$prefix_len = strlen($prefix);
-	if(strncmp($prefix, $class, $prefix_len) !== 0) {
-		return;
-	}
-	
-	$file = __DIR__.'/includes/classes'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $prefix_len)).'.php';
-	
-	if(file_exists($file) && is_file($file)) {
-		require_once $file;
-	}
-});
+// PSR-4 Autoloader
+require_once __DIR__.'/includes/autoload.php';
 
-// Alias classes
+// Put any alias classes here
 class theme extends \semantic\theme {}
 
 // Run any init methods here
---
+++ src/includes/autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Simple PSR-4 autoloader for the \semantic namespace
+ * 
+ * Loads files from `includes/classes`
+ * 
+ * @codeCoverageIgnore
+ */
+spl_autoload_register(function ($class) {
+	$prefix = 'semantic';
+	
+	$prefix_len = strlen($prefix);
+	if(strncmp($prefix, $class, $prefix_len) !== 0) {
+		return;
+	}
+	
+	$file = __DIR__.'/includes/classes'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $prefix_len)).'.php';
+	
+	if(file_exists($file) && is_file($file)) {
+		require_once $file;
+	}
+});
