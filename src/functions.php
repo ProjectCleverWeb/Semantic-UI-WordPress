96c25ba66bab2de46db210ffd404445257e96460
Nicholas Jordon <projectcleverweb@gmail.com>
2015-09-03 17:29:55 -0400

    fixed bug where function files were attempted to load twice

--- src/functions.php
+++ src/functions.php
@@ -23,11 +23,11 @@ spl_autoload_register(function ($class) {
 	}
 	
 	$search_path = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $prefix_len)).'.php';
-	$file_alt    = TEMPLATEPATH.$include_path.$search_path;
-	$file        = STYLESHEETPATH.$include_path.$search_path;
-	if(is_file($file_alt)) {
+	$file_alt    = realpath(TEMPLATEPATH.$include_path.$search_path);
+	$file        = realpath(STYLESHEETPATH.$include_path.$search_path);
+	if($file_alt && is_file($file_alt)) {
 		require_once $file_alt;
-	} elseif($file != $file_alt && is_file($file)) {
+	} elseif($file && $file != $file_alt && is_file($file)) {
 		require_once $file;
 	}
 });
@@ -42,9 +42,9 @@ set_query_var('debug',  $debug);
 
 /*** Functions (1 per file) ***/
 $debug->runtime_checkpoint('[Theme] Begin Including Function Files');
-$func_dir = $theme->include_path.'/function';
-$func_alt_dir = STYLESHEETPATH.'/'.$theme->include_sub_path.'/function';
-if (is_dir($func_alt_dir)) {
+$func_dir = realpath($theme->include_path.'/function');
+$func_alt_dir = realpath(STYLESHEETPATH.'/'.$theme->include_sub_path.'/function');
+if ($func_alt_dir && is_dir($func_alt_dir)) {
 	foreach (scandir($func_alt_dir) as $function_file) {
 		// ignore non-php files, check if the file exists, and that there isn't a function with the same name
 		if (substr($function_file, -4) == '.php' && is_file($func_alt_dir.DIRECTORY_SEPARATOR.$function_file) && !function_exists(substr($function_file, 0, -4))) {
