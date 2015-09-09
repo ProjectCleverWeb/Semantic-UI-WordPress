e884afa8bad4d8f442ec23f8b7204db8c66f5778
Nicholas Jordon <projectcleverweb@gmail.com>
2015-09-09 13:26:30 -0400

    simplified index.php logic without changing how it functions

--- src/index.php
+++ src/index.php
@@ -6,11 +6,8 @@
 if (is_404()) {
 	// There was an error with the request, display the 404 message instead of the normal loop
 	template_use_part($theme->content_sub_path.'/loop', $theme->content_sub_path.'/404');
-	template_part($theme->template_sub_path.'/default', 'page');
 } elseif (is_front_page() || is_home()) {
 	// This the home page, which is typically designed differently from the rest of the site
-	template_part($theme->template_sub_path.'/home');
-} else {
-	// None of the above, give them the default template for the type of page they are requesting
-	template_part($theme->template_sub_path.'/default', $theme->post_type);
+	template_use_part($theme->template_sub_path.'/default', $theme->template_sub_path.'/home');
 }
+template_part($theme->template_sub_path.'/default', $theme->post_type);
