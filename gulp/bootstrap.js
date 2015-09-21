aec919224c0423465b51a05bde3bde355f6491c8
Nicholas Jordon <projectcleverweb@gmail.com>
2015-09-21 10:17:45 -0400

    fixed issue where js concat used wrong directory

--- gulp/bootstrap.js
+++ gulp/bootstrap.js
@@ -2,18 +2,23 @@
  * Do any prep work here
  */
 
-var conf = require('./config');
+// Setup Vars
+var
+	conf     = require('./config'),
+	// Aliases
+	build    = conf.build,
+	paths    = build.paths;
 
 // Expand Source & Dist. Paths
-conf.build.paths.source_styles  = conf.build.paths.source + '/' + conf.build.paths.styles;
-conf.build.paths.source_scripts = conf.build.paths.source + '/' + conf.build.paths.scripts;
-conf.build.paths.dist_styles    = conf.build.paths.dist + '/' + conf.build.paths.styles;
-conf.build.paths.dist_scripts   = conf.build.paths.dist + '/' + conf.build.paths.scripts;
+paths.source_styles  = paths.source + '/' + paths.styles;
+paths.source_scripts = paths.source + '/' + paths.scripts;
+paths.dist_styles    = paths.dist + '/' + paths.styles;
+paths.dist_scripts   = paths.dist + '/' + paths.scripts;
 
 // Make concat paths relative to their respective directories
-for (i = 0; i < conf.build.concat_css_files.length; i++) { 
-	conf.build.concat_css_files[i] = conf.build.paths.dist_styles + '/' + conf.build.concat_css_files[i];
+for (i = 0; i < build.concat_css_files.length; i++) { 
+	build.concat_css_files[i] = paths.dist_styles + '/' + build.concat_css_files[i];
 }
-for (i = 0; i < conf.build.concat_js_files.length; i++) { 
-	conf.build.concat_js_files[i] = conf.build.paths.dist_styles + '/' + conf.build.concat_js_files[i];
+for (i = 0; i < build.concat_js_files.length; i++) { 
+	build.concat_js_files[i] = paths.dist_scripts + '/' + build.concat_js_files[i];
 }
