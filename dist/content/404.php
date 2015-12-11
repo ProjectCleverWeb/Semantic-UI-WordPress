3ff94dfa03ecc8abe9ffa5f517ae3c14739b9b07
ProjectCleverWeb <projectcleverweb@gmail.com>
2015-12-11 09:39:46 -0500

    updated 404 page to match new style

--- dist/content/404.php
+++ dist/content/404.php
@@ -1,21 +1,16 @@
-<div class="ui raised segment entry">
+<div class="ui basic segment entry">
 	<h2 class="ui large dividing header entry-title">
 		Error 404
 		<div class="sub header">The page you requested could not be found</div>
 	</h2>
 	<section class="ui basic segment entry-content">
 		<div class="ui middle aligned stackable grid">
-			<div class="one column row">
-				<div class="column">
-					<div class="ui center aligned header">
-						You Can...
-					</div>
+			<div class="row">
+				<div class="one wide column">
+					
 				</div>
-			</div>
-			<div class="two column row">
-				<div class="center aligned column">
-					Try to find page you were looking for: <br>
-					<div class="ui basic segment">
+				<div class="six wide center aligned column">
+					Try to find page you were looking for: <br><br>
 						<?php
 						/*
 						The wrapper is removed from the widget because not only is it not needed, but the bottom
@@ -24,13 +19,15 @@
 						*/
 						the_widget('WP_Widget_Search', array(), array('before_widget' => '', 'after_widget' => ''));
 						?>
-					</div>
 				</div>
-				<div class="center aligned column">
-					<div class="ui basic segment">
-						Go back to the homepage: <br><br>
-						<a class="small medium ui button" href="<?php echo home_url('/'); ?>">Go To Homepage</a>
-					</div>
+				<div class="one wide column">
+					
+				</div>
+				<div class="ui vertical divider">
+					OR
+				</div>
+				<div class="eight wide center aligned column">
+					<a class="ui large button" href="<?php echo home_url('/'); ?>">Go To Homepage</a>
 				</div>
 			</div>
 		</div>
--- dist/index.php
+++ dist/index.php
@@ -5,6 +5,7 @@
 
 if (is_404()) {
 	// There was an error with the request, display the 404 message instead of the normal loop
+	template_use_part($theme->template_sub_path.'/default', $theme->template_sub_path.'/default-page');
 	template_use_part($theme->content_sub_path.'/loop', $theme->content_sub_path.'/404');
 } elseif (is_front_page() || is_home()) {
 	// This the home page, which is typically designed differently from the rest of the site
