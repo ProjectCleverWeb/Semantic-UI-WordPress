e0c0128f4fadb42348e79ab8938e35a50d4917e3
ProjectCleverWeb <projectcleverweb@gmail.com>
2015-12-17 17:48:01 -0500

    fixed no comment section error for WP theme check

--- dist/content/loop.php
+++ dist/content/loop.php
@@ -152,7 +152,7 @@ if (have_posts()) {
 		
 		if (is_singular()) {
 			if ( comments_open() || 0 != get_comments_number()) :
-				comments_template();
+				comments_template('/'.$theme->content_sub_path.'/comments.php');
 			endif;
 		}
 		
