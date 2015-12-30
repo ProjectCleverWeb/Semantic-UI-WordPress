5f9223d063cf8f30a275c82eaa4df8b9814c38c9
ProjectCleverWeb <projectcleverweb@gmail.com>
2015-12-30 11:37:34 -0500

    fixed redundancy in theme section functions

--- src/include/function/theme_sidebar.php
+++ src/include/function/theme_sidebar.php
@@ -21,26 +21,5 @@
  * @return mixed       The returned value of the file on success, NULL otherwise
  */
 function theme_sidebar($name = NULL) {
-	global $debug, $theme;
-	$action = "get_sidebar";
-	$debug->runtime_checkpoint('[Theme] Action: '.$action);
-	
-	do_action($action, $name);
-	
-	$locations = array();
-	$name = (string) $name;
-	if (!empty($name)) {
-		$locations[] = "sidebar-{$name}";
-		$locations[] = $theme->content_sub_path."/sidebar-{$name}";
-	}
-	$locations[] = "sidebar";
-	$locations[] = $theme->content_sub_path."/sidebar";
-	
-	foreach ($locations as $loc) {
-		if (template_part__locate($loc.'.php')) {
-			return template_part($loc, $name);
-		}
-	}
-	$debug->runtime_checkpoint('[Theme] Failed Action: '.$action);
-	return NULL;
+	theme__section('sidebar', $name);
 }
