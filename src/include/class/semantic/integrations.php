1c23e39a69e9ee47fa9bbc69a4db057029f53ef8
Alex Filatov <alex@alexfilatov.com>
2019-02-23 12:45:30 +0000

    fixed theme after uprade to WP 5.1

--- src/include/class/semantic/integrations.php
+++ src/include/class/semantic/integrations.php
@@ -7,17 +7,17 @@ namespace semantic;
 
 /**
  * WordPress integrations class
- * 
+ *
  * This class class holds all the functions that integrate directly with WordPress
  */
 class integrations extends abstract_base {
-	
+
 	/**
 	 * Instance of the theme class
 	 * @var theme
 	 */
 	public $theme;
-	
+
 	/**
 	 * For whatever reason, we need to make sure it grabs the "right" theme global
 	 */
@@ -25,10 +25,10 @@ class integrations extends abstract_base {
 		$this->theme = $GLOBALS['theme'];
 		parent::__construct();
 	}
-	
+
 	/**
 	 * Registers various WordPress features
-	 * 
+	 *
 	 * @return void
 	 */
 	public function init() {
@@ -36,7 +36,7 @@ class integrations extends abstract_base {
 		global $content_width;
 		$theme         = $this->theme;
 		$content_width = 1200;
-		
+
 		// Tell WordPress what this theme supports
 		add_theme_support('automatic-feed-links');
 		add_theme_support('post-thumbnails');
@@ -61,13 +61,13 @@ class integrations extends abstract_base {
 			'default-position-x'     => 'center',
 			'default-attachment'     => 'scroll',
 		));
-		
+
 		// TIP: Use wp_nav_menu(array('theme_location' => 'menu-name')) to fetch these
 		register_nav_menus(array(
 			'main-menu'   => __('Main Menu', $theme::TEXT_DOMAIN),
 			'footer-menu' => __('Footer Menu', $theme::TEXT_DOMAIN)
 		));
-		
+
 		// The WP file editor is an abomination. May God help you if you or anyone
 		// in your company actually uses this.
 		if (in_array($GLOBALS['pagenow'], array('theme-editor.php'))) {
@@ -78,17 +78,17 @@ class integrations extends abstract_base {
 				), $theme::TEXT_DOMAIN).'</p>');
 			}
 		}
-		
+
 		// Remove WP's emoji
 		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 		remove_action( 'wp_print_styles', 'print_emoji_styles' );
 		remove_action( 'admin_print_styles', 'print_emoji_styles' );
 	}
-	
+
 	/**
 	 * Registers the theme widget areas.
-	 * 
+	 *
 	 * @return void
 	 */
 	public function widgets_init() {
@@ -121,10 +121,10 @@ class integrations extends abstract_base {
 			'after_title'   => '</h4>'
 		));
 	}
-	
+
 	/**
 	 * Registers all the theme styles/scripts
-	 * 
+	 *
 	 * @return void
 	 */
 	public function register_enqueue() {
@@ -151,10 +151,10 @@ class integrations extends abstract_base {
 		wp_register_script('theme-options', $this->theme->script_uri.'/theme-options.js', array(), NULL);
 		wp_register_script('base-concat', $this->theme->script_uri.'/base.concat.min.js', array(), NULL);
 	}
-	
+
 	/**
 	 * Enqueues the theme styles/scripts
-	 * 
+	 *
 	 * @return void
 	 */
 	public function enqueue() {
@@ -178,12 +178,12 @@ class integrations extends abstract_base {
 			wp_enqueue_script('comment-reply');
 		}
 	}
-	
+
 	/**
 	 * Registers the options page with WordPress, and enqueues style/scripts for
 	 * the options page in the dashboard. Only visible to users who can edit theme
 	 * options.
-	 * 
+	 *
 	 * @return void
 	 */
 	public function options() {
@@ -218,20 +218,20 @@ class integrations extends abstract_base {
 			);
 		}
 	}
-	
+
 	/**
 	 * Displays the options page content when called
-	 * 
+	 *
 	 * @return void
 	 */
 	public function options_page() {
 		template_part($this->theme->template_sub_path.'/theme-options');
 	}
-	
+
 	/**
 	 * This adds a "Theme Options" link to the WordPress admin bar under the menu
 	 * with the site name. Is only visible to users who can edit theme options.
-	 * 
+	 *
 	 * @param  object $wp_admin_bar The wp_admin_bar object as supplied by WordPress
 	 * @return void
 	 */
@@ -245,7 +245,7 @@ class integrations extends abstract_base {
 			));
 		}
 	}
-	
+
 	/**
 	 * Improve the existing wp_title()
 	 *
@@ -261,34 +261,34 @@ class integrations extends abstract_base {
 		$real_sep = trim($sep);
 		$sep      = ' '.$real_sep.' ';
 		$t_arr    = array();
-		
+
 		if (!empty($title)) {
 			$t_arr[] = $title;
 		}
-		
+
 		if (empty($real_sep)) {
 			$sep = ' ';
 		}
-		
+
 		if (is_feed()) {
 			return $title;
 		}
-		
+
 		// Add the blog name
 		$t_arr[] = get_bloginfo('name', 'display');
-		
+
 		// Add a page number if necessary:
 		if (($paged >= 2 || $page >= 2) && !is_404()) {
 			$t_arr[] = sprintf('Page %1$s', max($paged, $page));
 		}
-		
+
 		return implode($sep, $t_arr);
 	}
-	
+
 	/**
 	 * Adds a field to the user profile page so they can add their Google Plus URL
 	 * and be correctly marked as an author in posts they create
-	 * 
+	 *
 	 * @param  array    $profile_fields The contact fields array as provided by wordpress
 	 * @return string[]                 The resulting array after the field has been added.
 	 */
@@ -296,11 +296,11 @@ class integrations extends abstract_base {
 		$profile_fields['gplus'] = 'Google+ URL (for authorship)';
 		return $profile_fields;
 	}
-	
+
 	/**
 	 * Adds the theme's stylesheets to the post/page editor. This allows the visual
 	 * editor to more accurately represent what will be shown on the page.
-	 * 
+	 *
 	 * @return void
 	 */
 	public function editor_styles() {
@@ -309,10 +309,10 @@ class integrations extends abstract_base {
 		add_editor_style($this->theme->style_sub_path.'/webicons.min.css');
 		add_editor_style($this->theme->style_sub_path.'/main.css');
 	}
-	
+
 	/**
 	 * Adds the class "active" to the current menu item
-	 * 
+	 *
 	 * @param  array $classes The array of classes as given by WordPress
 	 * @return array          The modified array of classes
 	 */
@@ -322,10 +322,10 @@ class integrations extends abstract_base {
 		}
 		return $classes;
 	}
-	
+
 	/**
 	 * Replaces the default WordPress search form with one that uses Semantic UI.
-	 * 
+	 *
 	 * @return string The resulting form
 	 */
 	public function search_form() {
@@ -343,11 +343,11 @@ class integrations extends abstract_base {
 			(empty($query) ? 'Search...' : $query)
 		);
 	}
-	
+
 	/**
 	 * Replaces the default WordPress footer with one that has a paypal donation
 	 * link.
-	 * 
+	 *
 	 * @return void
 	 */
 	public function dashboard_footer() {
@@ -358,11 +358,11 @@ class integrations extends abstract_base {
 					Thank You!
 					<div class="sub header">If you found this WordPress theme useful, please consider donating:</div>
 				</h2>
-				
+
 				<p>
 					<?php
 					$fmt = '<a class="ui tiny basic inverted blue button" target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=%2$s">%1$s</a>'.PHP_EOL;
-					
+
 					printf($fmt, 'Donate $5', '2WLFNB3UMSELN');
 					printf($fmt, 'Donate $10', 'J42MM3FSZTPPQ');
 					printf($fmt, 'Custom Donation', 'DPSN8V5VVMHTA');
@@ -381,10 +381,10 @@ class integrations extends abstract_base {
 		</div>
 		<?php
 	}
-	
+
 	/**
 	 * Replaces the output of the_post_thumbnail()
-	 * 
+	 *
 	 * @param  string  $html          The orginal HTML (ignored)
 	 * @param  string  $post_id       The Post ID as provided by WordPress
 	 * @param  integer $post_image_id The Attachment ID as provided by WordPress
@@ -393,30 +393,31 @@ class integrations extends abstract_base {
 	public function post_thumbnail($html, $post_id, $post_image_id) {
 		$image = wp_get_attachment_image_src((integer) get_post_thumbnail_id((string) $post_id), 'single-post-thumbnail');
 		$alt   = get_post_meta($post_image_id, '_wp_attachment_image_alt');
-		
+
 		if (!isset($alt[0])) {
 			$alt = array('');
 		}
-		
+
 		return sprintf(
 			'<img itemprop="image" src="%1$s" alt="%2$s">',
 			$image[0],
 			trim(strip_tags($alt[0]))
 		);
 	}
-	
+
 	/**
 	 * Sets theme::$post_type
-	 * 
+	 *
 	 * @param  string $template The input string (ignored)
 	 * @return string           The input string (ignored)
 	 */
 	public function set_post_type($template) {
 		global $debug, $theme;
-		$theme->post_type = get_post_format();
-		if (empty($theme->post_type)) {
-			$theme->post_type = get_post_type();
+		$this->theme->post_type = get_post_format();
+		if (empty($this->theme->post_type)) {
+				$this->theme->post_type = get_post_type();
 		}
+		$theme = $this->theme;
 		$debug->runtime_checkpoint('[Theme] theme::$post_type set to "'.$theme->post_type.'"');
 		return $template;
 	}
